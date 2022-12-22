65d8a7a2770b0764ff22a4271318cb14856b0c58
Aboudoc <aboutika.reda@gmail.com>
2022-12-22 16:27:15 +0100

    initial commit

---
+++ scripts/flashbotsBundle.js
@@ -0,0 +1,32 @@
+const ethers = require("ethers")
+const { FlashbotsBundleProvider } = require("@flashbots/ethers-provider-bundle")
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+
+async function main() {
+    const authSigner = new ethers.Wallet(PRIVATE_KEY)
+    const provider = new ethers.providers.JsonRpcProvider(GOERLI_RPC_URL)
+    const flashbotsProvider = await FlashbotsBundleProvider.create(
+        provider,
+        authSigner
+    )
+
+    // Create Bundle
+    const signBundle = await flashbotsProvider.signBundle({
+        signer: SOME_SIGNER_TO_SEND_FROM,
+        transaction: SOME_TRANSACTION_TO_SEND,
+    })
+
+    const bundleReceipt = await flashbotsProvider.sendRawBundle(
+        signBundle,
+        TARGET_BLOCK_NUMBER
+    )
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
